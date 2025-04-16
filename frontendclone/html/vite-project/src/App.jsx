5379736c43814f79c0c6170a4735ecce81c97365
manishngp <manishngpdoc@gmail.com>
2025-04-16 23:32:24 +0530

    updated

--- frontendclone/html/vite-project/src/App.jsx
+++ frontendclone/html/vite-project/src/App.jsx
@@ -33,3 +33,78 @@ function App() {
 }
 
 export default App
+
+// import { useEffect, useState } from "react";
+// import "./styles.css";
+
+// export default function App() {
+//   const [posts, setPosts] = useState([]);
+//   const [serchTerm, setSerchTerm] = useState("");
+//   const [click, setClick] = useState("");
+//   const [currentPage, setcurrentPage] = useState(1);
+
+//   async function fetchData() {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+//       .then((res) => res.json())
+//       .then((data) => setPosts(data));
+//   }
+//   const handleSearch = (e) => {
+//     setSerchTerm(e.target.value);
+//     console.log(serchTerm);
+//   };
+//   console.log(serchTerm);
+
+//   const serch = () => {
+//     setClick(serchTerm.toLowerCase());
+//   };
+
+//   const filteredData = posts.filter((post) =>
+//     post.title.toLowerCase().includes(click)
+//   );
+
+//   const itemsPerPage = 5;
+
+//   let indexOfLastItem = currentPage * itemsPerPage;
+//   let indexOfFirstItem = indexOfLastItem - itemsPerPage;
+//   let pageFive = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+
+//   // const itemsPerPage = 5;
+
+//   // let indexOfLastItem = currentPage * itemsPerPage;
+//   // let indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+//   // let pageFive = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <input
+//         type="text"
+//         name="serchTerm"
+//         value={serchTerm}
+//         onChange={handleSearch}
+//       />
+//       <button onClick={serch}>search</button>
+
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>List of Posts</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {pageFive.map((item) => (
+//             <>
+//               <tr>
+//                 <td>{item.title}</td>
+//               </tr>
+//             </>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// }
