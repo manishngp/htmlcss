1f17292162b1617a23e99446803796009838c95b
manishngp <manishngpdoc@gmail.com>
2025-04-21 23:44:59 +0530

    new update

--- Athencation/src/components/Navbar.jsx
+++ Athencation/src/components/Navbar.jsx
@@ -15,3 +15,88 @@ const Navbar = () => {
   )
 }
 export default Navbar
+import React from 'react'
+import { NavLink } from 'react-router'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>LOGO</div>
+        <div>
+          <NavLink to="/">Home</NavLink>  
+          <NavLink to="/about">About</NavLink>  
+          
+        </div>
+    </div>
+
+  )
+}
+export default Navbar
+import React from 'react'
+import { NavLink } from 'react-router'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>LOGO</div>
+        <div>
+          <NavLink to="/">Home</NavLink>  
+          <NavLink to="/about">About</NavLink>  
+          
+        </div>
+    </div>
+
+  )
+}
+export default Navbar
+import React from 'react'
+import { NavLink } from 'react-router'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>LOGO</div>
+        <div>
+          <NavLink to="/">Home</NavLink>  
+          <NavLink to="/about">About</NavLink>  
+          
+        </div>
+    </div>
+
+  )
+}
+export default Navbar
+import React from 'react'
+import { NavLink } from 'react-router'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>LOGO</div>
+        <div>
+          <NavLink to="/">Home</NavLink>  
+          <NavLink to="/about">About</NavLink>  
+          
+        </div>
+    </div>
+
+  )
+}
+export default Navbar
+import React from 'react'
+import { NavLink } from 'react-router'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>LOGO</div>
+        <div>
+          <NavLink to="/">Home</NavLink>  
+          <NavLink to="/about">About</NavLink>  
+          
+        </div>
+    </div>
+
+  )
+}
+export default Navbar
