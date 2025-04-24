c130db6878ea62ef67f611dbf6cfa72f0f532e5f
manishngp <manishngpdoc@gmail.com>
2025-04-24 13:29:32 +0530

    some chages are done in 2nd question

--- apr24dsa/2nd.js
+++ apr24dsa/2nd.js
@@ -1,21 +1,20 @@
-function largestRectangleArea(heights) {
-    let stack = [];
-    let maxArea = 0;
+function largestArea(heights) {
+    let max = 0;
     heights.push(0); 
   
     for (let i = 0; i < heights.length; i++) {
 
       while (stack.length > 0 && heights[i] < heights[stack[stack.length - 1]]) {
-        
+
         const height = heights[stack.pop()];
         const width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
-        maxArea = Math.max(maxArea, height * width);
+        max = Math.max(max, height * width);
       }
       stack.push(i);
     }
   
-    return maxArea;
+    return max;
   }
   
   const heights = [2, 1, 5, 6, 2, 3];
-  console.log(largestRectangleArea(heights)); 
+  console.log(largestArea(heights)); 
