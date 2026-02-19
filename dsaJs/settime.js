19f01099e6e6176dce6d20301a0243ebe40f4a75
manishngp <manishngpdoc@gmail.com>
2026-02-19 18:20:09 +0530

    some practice

--- dsaJs/settime.js
+++ dsaJs/settime.js
@@ -1,1 +1,20 @@
-ref = {currentval:}
+let str = "Java 222222 Script!! Aa";
+
+str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+let freq = {};
+for (let ch of str) {
+  freq[ch] = (freq[ch] || 0) + 1;
+}
+
+let max = 0;
+for (let ch in freq) {
+  max = Math.max(max, freq[ch]);
+}
+
+let result = [];
+for (let ch in freq) {
+  if (freq[ch] === max) result.push(ch);
+}
+
+console.log(result, max); // ["a"] 4
