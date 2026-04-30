996dd9516a840d37b5aa6f2e6cb24562a5736dae
Manish Nagpure <92137867+manishngp@users.noreply.github.com>
2026-04-30 22:14:50 +0530

    Fix formatting in App.jsx by removing extra line

--- Athencation/src/App.jsx
+++ Athencation/src/App.jsx
@@ -1,6 +1,7 @@
-
+  
 import { Route, Router, Routes } from 'react-router'
-import './App.css'
+import './App.css' 
+
 import Navbar from './components/Navbar'
 import { Home } from './components/Home'
 import { About } from './components/About'
